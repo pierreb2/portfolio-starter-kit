12740a382164c33eb7e761f1639a27caf4de79d9
pierrebarat <pierre.barat@yahoo.com>
2025-12-10 16:41:21 +0100

    Update personal description in portfolio to reflect recent educational experience and enthusiasm for development.

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,11 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I am a 19-year-old French developer and a recent graduate from a computer science school in Paris,
-        where I studied several areas of software development, including low-level programming with C. I
-        have been passionate about technology since I was 13, and over the years I have built various
-        personal projects in web development as well as iOS and Android mobile applications.
-        Although I do not have professional experience yet, my studies and personal projects have helped
-        me develop a solid technical foundation and a strong willingness to learn.`}
+        {`I am a 19-year-old French developer and a recent graduate from 42 Paris,
+        where I had the opportunity to explore various areas of software development, including low-level programming with C.
+        I've been interested in technology since I was 13, and I've been working on some personal projects
+        in web development as well as iOS and Android mobile applications.
+        I'm eager to apply what I've learned and continue growing as a developer through real-world projects and collaboration.`}
       </p>
       {/* <div className="my-8">
         <BlogPosts />
