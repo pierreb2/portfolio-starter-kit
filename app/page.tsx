3590d491a6c7deeca003e708508d1e70800c591b
pierrebarat <pierre.barat@yahoo.com>
2025-12-10 17:17:01 +0100

    Enhance personal description in portfolio by adding language proficiency in French.

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,8 @@ export default function Page() {
         where I had the opportunity to explore various areas of software development, including low-level programming with C.
         I've been interested in technology since I was 13, and I've been working on some personal projects
         in web development as well as iOS and Android mobile applications.
-        I'm eager to apply what I've learned and continue growing as a developer through real-world projects and collaboration.`}
+        I'm eager to apply what I've learned and continue growing as a developer through real-world projects and collaboration.
+        I speak both English and French fluently.`}
       </p>
       {/* <div className="my-8">
         <BlogPosts />
