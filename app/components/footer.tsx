4e6e9bb75630f3bacbb409e1000a89feb0a37620
pierrebarat <pierre.barat@yahoo.com>
2025-12-10 16:21:40 +0100

    Add LinkedIn link to footer component

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -35,6 +35,17 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
+            href="https://www.linkedin.com/in/pierre-barat-533277355"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">linkedin</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
             href="https://github.com/pierreb2/portfolio-starter-kit"
           >
             <ArrowIcon />
